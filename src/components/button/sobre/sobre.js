0fb100dfae1f1868b71fad729dd2cadede72812b
Filipe <filipesoaresilva@gmail.com>
2026-03-24 21:02:22 -0300

    alteração no sobre

--- src/components/button/sobre/sobre.js
+++ src/components/button/sobre/sobre.js
@@ -1,20 +1,5 @@
 function Sobre() {
   return (
-<<<<<<< HEAD
-=======
-    /*<div>
-        <h1>👋 Ola ! Eu sou o Filipe, seja bem vindo ao meu Portifólio em React.JS !</h1>
-            <p className='sobre'>
-                Meu nome completo é Filipe Soares da Silva, nascido em 26 de junho de 1991 em Belém, Pará. Bacharel desde 2013 em Arquitetura e Urbanismo pela Universidade da Amazonia (UNAMA), atualmente estudando Ciência da Computação na Universidade Estácio de Sá, me dedicando ostensivamente para meu aprimoramento como profissional da área! Atualmente resido em Florianopolis,SC. Aqui vão algumas caracteristicas e habilidades minhas :
-            </p>
-        <div className='habilidades'>
-            <p> • Estudando atualmente focando no desenvolvimento das minhas habilidades em front-end (vue.js, react.js, angular.js).</p>
-            <p> • Fluente em inglês e português, com conhecimento basico também em francês.</p>
-            <p> • Comunicativo, sociável e curioso por natureza! Gosto de me desafiar e superar minhas limitações</p>
-            <p>• Boas praticas de programação, organização</p>
-        </div>
-    </div>*/
->>>>>>> 4753d68 (novas atualizações, 2026)
     <div>
       <h1>
         👋 Olá! Eu sou o Filipe, seja bem-vindo ao meu portfólio em React.js
