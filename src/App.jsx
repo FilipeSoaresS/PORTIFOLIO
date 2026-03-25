a045b6c74470db2b1f6d092703c6cb4f53f55dfa
Filipe <filipesoaresilva@gmail.com>
2026-03-24 21:00:04 -0300

    alteração da versão do node

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css';
-import Home from './index';
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@emotion/react';
+import "./App.css";
+import Home from "./index";
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@emotion/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 function App() {
